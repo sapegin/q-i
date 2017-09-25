1cf0d602b584cb2c59a2d839d03d8759a2c8059f
Artem Sapegin <artem@sapegin.ru>
2017-09-25 18:18:02 +0200

    Fix: Update TypeScript types after replacing Lodash with dlv

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,10 @@
-type Address = string | string[];
-
 interface Options {
 	maxItems?: number,
 }
 
 declare module 'q-i' {
 	declare function print(object: any, options?: Options) : void;
-	declare function print(object: any, addr?: Address, options?: Options) : void;
+	declare function print(object: any, addr?: string, options?: Options) : void;
 	declare function stringify(object: any, options?: Options) : string;
-	declare function stringify(object: any, addr?: Address, options?: Options) : string;
+	declare function stringify(object: any, addr?: string, options?: Options) : string;
 }
