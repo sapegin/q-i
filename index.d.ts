c771ffbde7262cbefab289daa6483fcb95c737e1
Artem Sapegin <artem@sapegin.ru>
2017-12-20 17:46:31 +0100

    Fix: Update TypeScript types

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,5 @@ interface Options {
 
 declare module 'q-i' {
 	declare function print(object: any, options?: Options) : void;
-	declare function print(object: any, addr?: string, options?: Options) : void;
 	declare function stringify(object: any, options?: Options) : string;
-	declare function stringify(object: any, addr?: string, options?: Options) : string;
 }
